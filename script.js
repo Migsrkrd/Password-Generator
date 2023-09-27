7fcbb63a4e5922aace01dd42c52c4b01cd4e8168
Mike <mreickcastillero@gmail.com>
2023-09-26 23:50:17 -0700

    added random element generators

--- script.js
+++ script.js
@@ -5,26 +5,51 @@ var lowerCase;
 var specialCharacter;
 var numbers;
 
+//testing zone_____________________________________________________________________________________
+var upperCaseOptions = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+var lowerCaseOptions = "abcdefghijklmnopqrstuvwxyz"
+var specialCharacterOptions = '!@#$%&*()+?'
+
+var numberOptionsCode = Math.floor(Math.random()*10); //random numbers
+console.log(numberOptionsCode);
+
+var lowerOptionsCode = lowerCaseOptions.charAt(Math.floor(Math.random()*lowerCaseOptions.length)); //random lower case letters
+console.log(lowerOptionsCode);
+
+var upperOptionsCode = upperCaseOptions.charAt(Math.floor(Math.random()*upperCaseOptions.length)) //random upper case letters
+console.log(upperOptionsCode);
+
+var specialOptionsCode = specialCharacterOptions.charAt(Math.floor(Math.random()*specialCharacterOptions.length)); //random special characters
+console.log(specialOptionsCode);
+
+
+//testing zone ____________________________________________________________________________________
+function generatePassword(){
+  password = ""
+var characters = prompt("how many characters do you want your password to have?");
+if (characters >= 8 && characters <= 128) {
+  upperCase = confirm("Would you like uppercase letters in your password?");
+  lowerCase = confirm("Would you like lowercase letters in your password?");
+  specialCharacter = confirm ("Would you like special characters in your password?");
+  numbers = confirm ("Would you like numbers in your password?");
+  var newArray = [characters, upperCase, lowerCase, specialCharacter, numbers];
+} else alert("Invalid Response, please try again");
+console.log(newArray);
+
+
+}
+
 // Write password to the #password input
 function writePassword() {
-  var characters = prompt("how many characters do you want your password to have?");
-  if (characters >= 8 && characters <= 128) {
-    upperCase = confirm("Would you like uppercase letters in your password?");
-    lowerCase = confirm("Would you like lowercase letters in your password?");
-    specialCharacter = confirm ("Would you like special characters in your password?");
-    numbers = confirm ("Would you like numbers in your password?");
-    var newArray = [characters, upperCase, lowerCase, specialCharacter, numbers];
-  } else alert("Invalid Response, please try again")
- 
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
 
-  //  var password = generatePassword();
-  //  var passwordText = document.querySelector("#password");
-
-  // passwordText.value = password;
-
-
-console.log(newArray);
+  passwordText.value = password;
 }
+
+
+
+
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
