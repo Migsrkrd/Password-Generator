555305095b65ea7356107f801900213a6d430fea
Mike <mreickcastillero@gmail.com>
2023-09-28 00:10:37 -0700

    my original method was terrible so I restarted and applied arrays to conditions sequentially

--- script.js
+++ script.js
@@ -4,40 +4,117 @@ var upperCase
 var lowerCase;
 var specialCharacter;
 var numbers;
+var characters;
+
 
 //testing zone_____________________________________________________________________________________
 var upperCaseOptions = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
 var lowerCaseOptions = "abcdefghijklmnopqrstuvwxyz"
-var specialCharacterOptions = '!@#$%&*()+?'
+var specialCharacterOptions = "!\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~"
+var numberOptions = "0123456789"
 
-var numberOptionsCode = Math.floor(Math.random()*10); //random numbers
-console.log(numberOptionsCode);
+var lowerCaseArray = [];
+for(var i=0;i<lowerCaseOptions.length;i++){
+  lowerCaseArray[i] = lowerCaseOptions.slice(i,i+1);
+} 
+// lowerCaseArray ------------------
 
-var lowerOptionsCode = lowerCaseOptions.charAt(Math.floor(Math.random()*lowerCaseOptions.length)); //random lower case letters
-console.log(lowerOptionsCode);
-
-var upperOptionsCode = upperCaseOptions.charAt(Math.floor(Math.random()*upperCaseOptions.length)) //random upper case letters
-console.log(upperOptionsCode);
-
-var specialOptionsCode = specialCharacterOptions.charAt(Math.floor(Math.random()*specialCharacterOptions.length)); //random special characters
-console.log(specialOptionsCode);
-
-
-//testing zone ____________________________________________________________________________________
-function generatePassword(){
-  password = ""
-var characters = prompt("how many characters do you want your password to have?");
-if (characters >= 8 && characters <= 128) {
-  upperCase = confirm("Would you like uppercase letters in your password?");
-  lowerCase = confirm("Would you like lowercase letters in your password?");
-  specialCharacter = confirm ("Would you like special characters in your password?");
-  numbers = confirm ("Would you like numbers in your password?");
-  var newArray = [characters, upperCase, lowerCase, specialCharacter, numbers];
-} else alert("Invalid Response, please try again");
-console.log(newArray);
-
-
+var upperCaseArray = [];
+for(var i=0;i<upperCaseOptions.length;i++){
+  upperCaseArray[i] = upperCaseOptions.slice(i,i+1);
 }
+
+var specialCharacterArray = [];
+for(var i=0;i<specialCharacterOptions.length;i++){
+  specialCharacterArray[i] = specialCharacterOptions.slice(i,i+1);
+}
+ var numberArray = [];
+ for(var i=0;i<numberOptions.length;i++) {
+  numberArray[i] = numberOptions.slice(i,i+1);
+ }
+
+
+
+
+
+
+ //testing zone ____________________________________________________________________________________
+ function validate(){
+   var characters = prompt("how many characters do you want your password to have?");
+   if (characters >= 8 && characters <= 128) {
+   upperCase = confirm("Would you like uppercase letters in your password?");
+   lowerCase = confirm("Would you like lowercase letters in your password?");
+   specialCharacter = confirm ("Would you like special characters in your password?");
+   numbers = confirm ("Would you like numbers in your password?");
+   } else {
+    alert("Invalid Response, please try again");
+    validate();
+  }
+  var totalArray = [];
+  if(upperCase){
+ totalArray = totalArray.concat(upperCaseArray);
+  }
+  if(lowerCase){
+    totalArray = totalArray.concat(lowerCaseArray);
+  }
+  if(specialCharacter) {
+    totalArray = totalArray.concat(specialCharacterArray);
+  }
+  if(numbers){
+    totalArray = totalArray.concat(numberArray);
+  }
+  console.log(totalArray);
+ }
+
+ function generatePassword(){
+  validate();
+  // var passwordArray = [];
+  // totalArray = [];
+  // for(i=0;i<characters;i++){
+  // passwordArray[i] = totalArray.At(Math.floor(Math.random()*totalArray.length));
+  // }
+  // console.log(passwordArray[i]);
+  //  var password = passwordArray.join("");
+  //  console.log(password)
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  // if (characters && upperCase && lowerCase && specialCharacter && numbers){
+  //   allTrue = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz!@#$%&*()+?0123456789";
+  
+  
+   
+    
+
+
+      
+
 
 // Write password to the #password input
 function writePassword() {
